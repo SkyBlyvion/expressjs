559b8311f4333c00199bc53ec879271839efe822
Lopez Nicolas <richtofen60@gmail.com>
2024-02-26 09:32:59 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+//Landing page
+//1st argument : url , 2nd argument : fonction
+//entrypoint / fonction annonyme, request et response
+app.get('/', (req,res)=>{
+    res.send('Hello World');
+})
+
+// test page
+app.get('/test', (req,res)=>{
+    res.send('Hello test');
+})
+
+
+// Listen on port 3000
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port http://localhost:${PORT}`);})
