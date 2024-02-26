3b5524fd66de614e067332018215d49a2df9b974
Lopez Nicolas <richtofen60@gmail.com>
2024-02-26 11:46:29 +0100

    add Header/footer, add templates, typo postRoute(s)

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
+// import des modules
 const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+// init application
 const app = express();
 
-//Landing page
-//1st argument : url , 2nd argument : fonction
-//entrypoint / fonction annonyme, request et response
-app.get('/', (req,res)=>{
-    res.send('Hello World');
-})
+// Connexion to MongoDB
+//TODO: do the connexion to mongoDB
 
-// test page
-app.get('/test', (req,res)=>{
-    res.send('Hello tesaat');
-})
+// Session config
+app.use(session({
+    secret: 'user_info', // Secret Key for data crypt
+    resave: true, // save the session 
+    saveUninitialized: true // save empty sessions
+}))
 
+// bodyparser : middleware pour parser les requêtes
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
+// Passport config
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes Configuration
+const authRoutes = require('./routes/authRoute');
+const postRoutes = require('./routes/postRoute');
+const userRoutes = require('./routes/userRoute');
+
+app.use('/', authRoutes);
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
+
+// config view engine
+app.set('view engine', 'ejs');
+app.set('views', './view');
 
 // Listen on port 3000
 const PORT = 3000;
